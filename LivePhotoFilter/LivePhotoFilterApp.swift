09bad03a40713852b27f0c67632ae3786c3b37d9
torhovenkov <vladislav.lnx@gmail.com>
2023-07-11 05:41:20 +0300

    Initial Commit

---
+++ LivePhotoFilter/LivePhotoFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivePhotoFilterApp.swift
+//  LivePhotoFilter
+//
+//  Created by Vladyslav Torhovenkov on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct LivePhotoFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
